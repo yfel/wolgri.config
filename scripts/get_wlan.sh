31042a3267fe671587e241f5c0c6b21dc6c65680
Volodymer Grigorenko <wolgri@gmail.com>
2009-06-11 16:53:32 +0300

    fix wlan script

--- scripts/get_wlan.sh
+++ scripts/get_wlan.sh
@@ -1,6 +1,7 @@
-
-/sbin/iwconfig wlan0 |sed -e 's/[:=\/\"]/ /g'|awk '
- /Mode/ {print "Point: " $8":"$9":"$10":"$11":"$12":"$13} 
+#!/bin/sh
+WLAN=ath0
+/sbin/iwconfig $WLAN |sed -e 's/[:=\/\"]/ /g'|awk '
+ /ESSID/ {print "ESSID: " $5} 
  /Bit Rate/ {print "Rate: " $3" "$4"/"$5}
  /Link Quality/ {print "Quality:" ($3/$4)*100}' 
 
